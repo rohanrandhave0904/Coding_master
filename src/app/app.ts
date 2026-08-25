7e14277d49c14046e7dbfb753d28a85e48a9eeed
DESKTOP-8QLMABP\Admin <rohanrandhave1212@gmail.com>
2026-08-25 23:30:34 +0530

    rohan-randhave--->rakshabandhan for someone

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('Coding_master');
+  showSurprise = false;
+  hugging = false;
+
+  brotherName = 'ROHAN';
+  sisterName = 'POOJA';
+
+  openSurprise() {
+    this.showSurprise = !this.showSurprise;
+
+    if (this.showSurprise) {
+      this.hugging = true;
+
+      setTimeout(() => {
+        this.hugging = false;
+      }, 2200);
+    }
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
 }
